ce9a16963eeb0c8c9994f1ab3748d82dd0f38fc9
davesag <davesag@gmail.com>
2018-06-04 10:08:06 +0530

    n/a removed a few unused dependnecies

--- test/unit/test_helper.js
+++ test/unit/test_helper.js
@@ -1,8 +1,6 @@
 const chai = require('chai')
 const sinonChai = require('sinon-chai')
 const chaiAsPromised = require('chai-as-promised')
-const chaiString = require('chai-string')
 
 chai.use(sinonChai)
 chai.use(chaiAsPromised)
-chai.use(chaiString)
