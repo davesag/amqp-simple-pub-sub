b829725d2966f417aa9a242b9c97a24e4bc995ed
Dave Sag <davesag@gmail.com>
2019-05-19 16:38:13 +0530

    n/a udpated stryker

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,6 +1,5 @@
 module.exports = function(config) {
   config.set({
-    maxConcurrentTestRunners: 2, // workaround for https://github.com/stryker-mutator/stryker/issues/1525
     mutate: [
       'src/**/*.js',
       '!src/defaults.js',
@@ -9,7 +8,7 @@ module.exports = function(config) {
     ],
     mutator: 'javascript',
     packageManager: 'npm',
-    reporters: ['clear-text', 'progress'],
+    reporters: ['clear-text'],
     testRunner: 'mocha',
     mochaOptions: {
       spec: ['./test/unit/**/*.test.js'],
